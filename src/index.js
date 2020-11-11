b9c7188c30f75aef9996daa88f1ace370fe9e7ec
Thor Hlavaty <thorthebore@gmail.com>
2020-11-11 10:56:42 -0500

    banking app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { createStore } from 'redux';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -14,4 +17,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+console.log('Starting Banking App')
